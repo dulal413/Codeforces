9c286c9e56923d45939a48d6ff02fddbbc71cfd9
Ankit Podder <ankitpodder0211@gmail.com>
2021-10-08 02:16:25 +0530

    Done day8

---
+++ Day8/CustomizingTrack.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define lli long long int
+using namespace std;
+
+class Solution{
+    public:
+    lli solve(lli n , lli sum){
+        if(sum % n == 0) return 0;
+        lli ans = 0;
+        lli v1 = sum % n;
+        lli v0 = n - v1;
+        return v0 * v1;
+    }
+};
+
+int main(){
+    // #ifndef ONLINE_JUDGE
+    //     freopen("input.txt", "r", stdin);
+    // #endif
+
+    lli t;
+    cin >> t;
+
+    while(t--){
+        lli n , sum=0 , val;
+        cin >> n;
+        for(lli i = 0 ; i < n ; i++) {
+            cin >> val;
+            sum += val;
+        }
+        Solution obj;
+        cout << obj.solve(n , sum) << endl;
+    }
+    return 0;
+}
